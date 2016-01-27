bce5da4a3dc0baea96756b8837f04538f20449ed
Diego Sanchez <diego.sanches@gmail.com>
2016-01-27 11:27:47 -0300

    [setup] Initial commit
    setting up environment as well as CI integration

---
+++ README.md
@@ -0,0 +1,3 @@
+# GCloud based time autoscaling
+
+It's a based time autoscaler for gcloud platform
---
+++ test/gautoscaler.js
@@ -0,0 +1,20 @@
+var gautoscaler = require("../lib/gautoscaler");
+
+describe("gautoscaler", function() {
+
+    describe("user record an event", function() {
+
+        it("shouldn't add events with same date (start and end)", function(done) {
+            done();
+        })
+    });
+});
+
+
+
+
+
+
+
+
+
